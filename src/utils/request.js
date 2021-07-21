d406cd5864d4add9918c9f60911bc0e902d1001c
huanglin <2762565371@qq.com>
2021-07-21 22:08:00 +0800

    创建登录页面并配置组件路由

---
+++ src/utils/request.js
@@ -0,0 +1,17 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// 创建一个 axios 实例
+// 我们通过实例去发请求，把需求配置给这个实例
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基本路径
+})
+
+// 请求拦截器
+
+// 响应拦截其
+
+// 导出请求的request实例
+export default request
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login-container">登录页面</div>
+</template>
+<script>
+export default {
+  name: 'LoginIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  cteated () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
