035042295be7c5eaeae4c0da05a0ed6915a6674a
huanglin <2762565371@qq.com>
2021-07-21 21:02:00 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载Element组件库
+import ElementUI from 'element-ui'
+
+// 加载Element组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 全局注册Element组件库
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
