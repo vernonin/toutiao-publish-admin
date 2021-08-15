0a4e48e1496c83d06e30edfe5cc68113dff56eeb
huanglin <2762565371@qq.com>
2021-08-15 21:34:39 +0800

    完成素材管理页面

--- src/api/image.js
+++ src/api/image.js
@@ -11,3 +11,28 @@ export const uploadImg = data => {
     data
   })
 }
+// 获取用户图片素材列表
+export const getImages = params => {
+  return request({
+    method: 'GET',
+    url: '/mp/v1_0/user/images',
+    params
+  })
+}
+// 收藏素材图片
+export const collectImage = (imageId, collect) => {
+  return request({
+    method: 'PUT',
+    url: `/mp/v1_0/user/images/${imageId}`,
+    data: {
+      collect
+    }
+  })
+}
+// 删除素材图片
+export const deleteImage = imageId => {
+  return request({
+    method: 'DELETE',
+    url: `/mp/v1_0/user/images/${imageId}`
+  })
+}
